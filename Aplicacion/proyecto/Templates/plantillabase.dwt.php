ef409550c06436a9759368d280622a57217e8b7f
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-07-16 18:01:45 -0500

    recopilando informacion

---
+++ Aplicacion/proyecto/Templates/plantillabase.dwt.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Documento sin t&iacute;tulo</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<link href="../css/estilo.css" rel="stylesheet" type="text/css" />
+<?php include("includes/google.php"); ?>
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header"><a href="#">
+  <?php include("includes/cabecera.php"); ?>
+  <?php include("includes/menu.php"); ?>
+  </a> 
+    <!-- end .header --></div>
+  <div class="sidebar1">
+    
+    <p> informacion.</p>
+    <p>informacion 2.</p>
+    <!-- end .sidebar1 --></div>
+  <div class="content">
+    <h1>Instrucciones</h1>
+    <p>informacion 3</a>.</p>
+    <h2>M&eacute;todo de borrado</h2>
+    <p>informacion 3</p>
+    <p>&nbsp;</p>
+    <h3>Sustituci&oacute;n de logotipo</h3>
+    <p>info 5</p>
+    <h4>Fondos</h4>
+    <p>info 6</p>
+    <p>&nbsp;</p>
+    <!-- end .content --></div>
+  <div class="footer">
+  <?php include("includes/pie.php"); ?>
+  </p>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
