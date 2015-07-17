af89d8898cdfd666ff200c69fa287efdf8b86eb9
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-07-17 02:40:48 -0500

    informacion de rutas

--- Aplicacion/proyecto/index.php
+++ Aplicacion/proyecto/index.php
@@ -17,23 +17,36 @@
 <div class="container">
   <div class="header"><a href="#">
   <?php include("includes/cabecera.php"); ?>
+  <div class"clearfloat"></div>
   <?php include("includes/menu.php"); ?>
   </a> 
     <!-- end .header --></div>
   <div class="sidebar1">
     
-    <p> informacion.</p>
-    <p>informacion 2.</p>
+    <p> CALENDARIO TURISTICO 2015.</p>
+    <p>RUTAS TURISTICAS.</p>
     <!-- end .sidebar1 --></div>
   <div class="content">
-    <h1>Instrucciones</h1>
-    <p>informacion 3</a>.</p>
-    <h2>M&eacute;todo de borrado</h2>
-    <p>informacion 3</p>
+    <h1>ANDAHUAYLAS</h1>
+    <p>La ciudad de Andahuaylas es capital de la provincia de del mismo nombre, es una de las 7 provincias que conforman el Departamento de Apurimac. Es tambien conocida como la tierra de los celajes (por el color del cielo al atardecer) y la cultura CHANKA.
+
+La ciudad se encuentra sobre los 2,980 metros sobre el nivel del mar y posee un clima ligeramente frio. En su Plaza de Armas destaca la Pileta construida de una sola piedra y la Catedral de San Pedro que fue construida en la Epoca Colonial.
+
+A continuacion un resumen de los principales atractivos que encontrara en esta provincia del departamento de Apurimac, los cuales podran apreciar con mas detalle en las siguientes paginas.
+
+Nota: Se ha colocado en primer lugar la Provincia de de Andhuaylas debido a que la carretera que une estos pueblos partiendo desde Ayacucho, pasa por Andahuaylas, luego Abancay, para posteriormente arribar al Cusco, que es el punto de llegada de muchos turistas.</a>.</p>
+    <h2>LA RUTA ANDAHUAYLAS ABANCAY</h2>
+    <p>Esta ruta es parte de una gran ruta, que finaliza en la ciudad de Cusco, el tramo comprendido entre Andahuaylas y Abancay, es un tramo increible, en el que encontraras enormes cadenas de cerros, precipicios verticales y extensos sembrios de papa huayro.
+
+La ruta es sobre una carretera afirmada, donde abunda el polvo en temporadas de seca y el barro en temporada de lluvia.
+
+Esta ruta es cubierta por un par de empresas locales, no obstante es tambien usado por otras empresas que cubren rutas interdepartamentales, el tiempo de viaje entre estas localidades es 06 horas aproximadamente; si no se presenta ningun contratiempo, existiendo salidas en las mañanas, al medio dia y en la noche.</p>
     <p>&nbsp;</p>
-    <h3>Sustituci&oacute;n de logotipo</h3>
-    <p>info 5</p>
-    <h4>Fondos</h4>
+    <h3>Hoteles en Andahuaylas - Vacaciones</h3>
+    <p>- La Mansion Casa Hotel
+    - Hotel Conquistador - Imperio Chanka
+    </p>
+    <h4>Restaurants en Andahuaylas</h4>
     <p>info 6</p>
     <p>&nbsp;</p>
     <!-- end .content --></div>
